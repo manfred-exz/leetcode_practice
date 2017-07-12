c5e5a2a486896689aa54d9fff2291ba898f71fd5
manfred-exz <manfred.exz@gmail.com>
2017-07-12 16:26:11 +0800

    pass "maximum subarray" unfinished "matchsticks to square"

--- Matchsticks to Square.cpp
+++ Matchsticks to Square.cpp
@@ -16,15 +16,18 @@
 #include <functional>
 #include <chrono>
 #include <boost/format.hpp>
+#include <set>
+#include <map>
 
 using namespace std;
 class Solution {
+    constexpr static int num_of_lines = 4;
+
     std::vector<std::vector<int>> lines;
-    size_t num_of_lines;
     int total_sum;
     int line_sum;
 public:
-    Solution() : num_of_lines(4) {
+    Solution() {
         lines.resize(num_of_lines);
     }
     bool all_line_has_same_length() const {
@@ -37,27 +40,66 @@ public:
         lines.resize(4);
         total_sum = line_sum = 0;
     }
+    int least_length_needed() const {
+        std::array<int, num_of_lines> line_lengths;
+        for (int i = 0; i < line_lengths.size(); ++i) {
+            line_lengths[i] = std::accumulate(lines[i].begin(), lines[i].end(), 0);
+        }
 
+        auto max_iter = std::max_element(line_lengths.begin(), line_lengths.end());
+        int needed = 0;
+        for(auto iter = line_lengths.begin(); iter != line_lengths.end(); ++iter){
+            if(iter != max_iter)
+                needed = needed + (*max_iter - *iter);
+        }
+        return needed;
+    }
+    void sort_lines(){
+        std::sort(lines.begin(), lines.end(), [](const std::vector<int>& lhs, const std::vector<int>& rhs){
+           return std::accumulate(lhs.begin(), lhs.end(), 0) < std::accumulate(rhs.begin(), rhs.end(), 0);
+        });
+    }
 public:
-    bool makesquare(const vector<int>& nums) {
+    bool makesquare(vector<int> nums) {
         reset();
+
+        if(nums.size() < 4)
+            return false;
+
         total_sum = std::accumulate(nums.begin(), nums.end(), 0);
-//        if(total_sum % num_of_lines != 0)
-//            return false;
+        if (total_sum % num_of_lines != 0) {
+            std::cout << boost::format("[total_sum is %d, not mutiplies of %d, reminder is %d]\n")
+                         % total_sum
+                         % 4
+                         % (total_sum%num_of_lines);
+            return false;
+        }
         line_sum = total_sum/ static_cast<int>(num_of_lines);
+
+
+
         auto b = recursive(nums.begin(), nums.end());
         return b;
     }
 
-    bool recursive(vector<int>::const_iterator begin, vector<int>::const_iterator end){
-        if(begin == end)
+    bool recursive(vector<int>::iterator begin, vector<int>::iterator end) {
+        int rest_element = std::distance(begin, end);
+
+        // check if success
+        if (rest_element == 0)
             return all_line_has_same_length();
 
+        // pruning
+        int unchecked_number_sum = std::accumulate(begin, end, 0);
+        if (unchecked_number_sum < least_length_needed())
+            return false;
+
         bool success = true;
+//        sort_lines();
         for (int i = 0; i < lines.size(); ++i) {
-            auto& line = lines[i];
+            auto &line = lines[i];
             line.push_back(*begin);
-            if(recursive(begin+1, end) == false) {
+            if (recursive(begin + 1, end) == false) {
                 success = false;
             } else {
                 return true;
@@ -68,11 +110,7 @@ public:
     }
 };
 
-void print_res(bool b){
-    std::cout << b << std::endl;
-}
-
-void benchmark(Solution& s, const std::vector<int>& input, bool expected){
+void benchmark(Solution& s, std::vector<int> input, bool expected){
     using namespace std::chrono;
     auto start_time = system_clock::now();
     bool res = s.makesquare(input);
@@ -92,6 +130,9 @@ int main(){
     benchmark(s, {1, 1, 1, 1}, true);
     benchmark(s, {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, true);
     benchmark(s, {1, 1, 1, 1, 1, 1, 1, 1, 1}, false);
+
+    benchmark(s, {5969561, 8742425, 2513572, 3352059, 9084275, 2194427, 1017540, 2324577, 6810719, 8936380,
+                  7868365, 2755770, 9954463, 9912280, 4713511}, false);
 //
 //    print_res(s.makesquare({2, 3, 4, 5, 1, 2, 3, 298, 32, 123, 123, 723, 1234,
 //                            8713, 1278, 12378, 34728, 23748,78, 234781,4718347, 1427384
