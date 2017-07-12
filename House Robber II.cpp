bf82986c0127656bce81d60f509bb5fda2ff5bd4
manfred-exz <manfred.exz@gmail.com>
2017-07-12 17:14:33 +0800

    pass "House Robber II" simplify "House Robber"

--- House Robber II.cpp
+++ House Robber II.cpp
@@ -4,15 +4,15 @@
 #include <vector>
 #include <cassert>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    int rob(vector<int> nums) {
+    int rob(vector<int> nums){
         if(nums.size() == 0) return 0;
         if(nums.size() == 1) return nums[0];
-
 
         // dp[i] is the max value you can rob if house i is the last house you can rob(but you can choose not to rob)
         std::vector<int> dp(nums.size());
@@ -23,7 +23,18 @@ public:
             dp[i] = std::max(nums[i] + dp[i-2], dp[i-1]);
         }
 
-        return dp.back();
+        std::reverse(nums.begin(), nums.end());
+
+        // reverse_dp[i] is the max value you can rob if house i is the last house you can rob(but you can choose not to rob)
+        std::vector<int> reverse_dp(nums.size());
+        reverse_dp[0] = nums[0];
+        reverse_dp[1] = std::max(reverse_dp[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); ++i) {
+            reverse_dp[i] = std::max(nums[i] + reverse_dp[i - 2], reverse_dp[i - 1]);
+        }
+
+        return std::max(dp[dp.size()-2], reverse_dp[reverse_dp.size()-2]);
     }
 };
 
